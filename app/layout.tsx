283883dfa6174cc3ed07ef006d74ef3d04ff7df2
Sanjay Soni <sanjaysoni2k1@gmail.com>
2026-03-26 14:02:56 +1100

    Updated the Theme and Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
 import type { Metadata } from "next"
-import { Inter, Source_Serif_4, JetBrains_Mono } from "next/font/google"
+import { Sora, Roboto, Fira_Code } from "next/font/google";
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 
-const fontSans = Inter({
+const fontSans = Roboto({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
-const fontSerif = Source_Serif_4({
+const fontSerif = Sora({
   subsets: ["latin"],
   variable: "--font-serif",
-})
+});
 
-const fontMono = JetBrains_Mono({
+const fontMono = Fira_Code({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+});
 
 export const metadata: Metadata = {
   title: "Helix",
@@ -30,9 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontSerif.variable} ${fontMono.variable} antialiased`}
-      >
+      <body className={`${fontSans.variable} ${fontSerif.variable} ${fontMono.variable} antialiased`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
